e02075b3ac4a6e23f839a6a71cd545a63bdac2ab
MamunHossain1999 <md.mamun.hossain.bj@gmail.com>
2024-09-24 02:14:15 +0600

    added file

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 document.getElementById('donate').addEventListener('click', function(){
-       
-    const moneyAdd = document.getElementById('add_balance').innerText;
-    const rebens = document.getElementById('rebens-balance').innerText;
-    console.log( moneyAdd, rebens);
-});
+    const donate = parseFloat(document.getElementById('donation').value);
+    const balance = parseFloat(document.getElementById('add-balance').innerText);
+    const myAccount = parseFloat(document.getElementById('myAccount').innerText);
+   
+    // console.log(donate, balance, myAccount);
+    if(isNaN(donate)){
+        alert('invalid amount')
+        return;
+    }
+    if( donate <= 0){
+    alert('invalid amount')
+    return;
+    }
+    const newBalance = myAccount - donate;
+   
+// 
 
+if(newBalance < 0){
+    alert('not enough amount')
+    return;
+}
+document.getElementById('add-balance').innerText = donate + balance;
 
-  
+document.getElementById('myAccount').innerText =  newBalance;
+
+my_modal_5.showModal()
+document.getElementById('donate').value = '';
+})
