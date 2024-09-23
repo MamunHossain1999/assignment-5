2594e4fd9d537fd3f35dc38c8037403dbd438974
MamunHossain1999 <md.mamun.hossain.bj@gmail.com>
2024-09-23 20:07:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+document.getElementById('donate').addEventListener('click', function(){
+       
+    const moneyAdd = document.getElementById('add_balance').innerText;
+    const rebens = document.getElementById('rebens-balance').innerText;
+    console.log( moneyAdd, rebens);
+
+
+    // const inputField = document.getElementById('donation');
+    // console.log(inputField.value)
+    // const inputTaxt = inputField.value;
+   
+    // const balance = document.getElementById('add_balance');
+    // balance.innerText = inputTaxt;
+    // console.log(balance)
+    // inputField.value = '';
+    // if(donation === '10000'){
+    //     console.log('how are you')
+    //     const balance = document.getElementById('add-balance').innerText;
+    //     console.log(balance)
+
+    //     const revens =parseFloat(rebens);
+    //     const addNewMoney = parseFloat(balance);
+    //     console.log( revens, addNewMoney)
+
+    //     const newBalance = revens - addNewMoney;
+    //     console.log( newBalance);
+
+
+    //     document.getElementById('add-balance').value = newMoney;
+    // }
+    // else{
+    //     alert('not a number')
+    // }
+});
+
+
+    // if(password === '1234'){
+    //     console.log('you are logged');
+
+        // const balance = document.getElementById('account-balance').innerText;
+
+        // value check korar jonno typeof use kora hoy
+        // console.log(balance);
+
+        // const newBalance = balance + money
+        // kono string k number convart korar jonno parseFloat use kora hoy;
+        // kono vognangso songkhake purno songkhay ante parseIntejar use kora hoy
+        // const addMoney = parseFloat(money);
+        // const addNewMoney = parseFloat(balance);
+        // console.log(addMoney,  addNewMoney );
+
+        // const newBalance = addMoney + addNewMoney;
+        // console.log(newBalance)
+
+        // document.getElementById('account-balance').innerText = newBalance
+    
+    // else{
+    //     alert('please try your latter')
+    // };
