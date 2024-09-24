e5db956db61f0b0c9bc08b6e970ac98f52bf0eb8
MamunHossain1999 <md.mamun.hossain.bj@gmail.com>
2024-09-24 18:30:33 +0600

    added new card

--- toggole-freture.js
+++ toggole-freture.js
@@ -1,21 +1,7 @@
-document.getElementById('btn').addEventListener('click', function(){
-    console.log('click the show botton')
 
-    document.getElementById('btn').classList.add('hidden');
 
+document.getElementById('main').addEventListener('click', function(){
+    console.log('click the show button');
+
+    showSection('myAccount');
 });
-document.getElementById('btn').addEventListener('click', function(){
-    console.log('click the show botton')
-
-    document.getElementById('btn').classList.remove('hidden');
-
-});
-document.getElementById('btn1').addEventListener('click', function(){
-    console.log('click the show ')
-
-
-    
-
-    document.getElementById('btn1').classList.remove('hidden');
-    document.getElementById('btn1').classList.add('hidden');
-})
